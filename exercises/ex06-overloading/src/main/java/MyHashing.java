6cb70bde0deb1a9bbac011c781c76dcf94e599ce
JK <janica.krznaric@mail.utoronto.ca>
2026-09-21 10:13:53 -0400

    implemented hashing excercise 5

--- exercises/ex06-overloading/src/main/java/MyHashing.java
+++ exercises/ex06-overloading/src/main/java/MyHashing.java
@@ -19,7 +19,6 @@ public class MyHashing {
 
   /** Creates a MyHashing whose seed starts at 0. */
   public MyHashing() {
-    // TODO: this constructor takes no arguments; leave the seed at its default.
   }
 
   /**
@@ -28,7 +27,7 @@ public class MyHashing {
    * @param seed the initial seed value
    */
   public MyHashing(int seed) {
-    // TODO: store the parameter in this object's seed field.
+    this.seed = seed;
   }
 
   /**
@@ -38,8 +37,9 @@ public class MyHashing {
    * @return the seed value from before this call
    */
   public int hash(int value) {
-    // TODO
-    return 0;
+    int prev = this.seed;
+    this.seed = value;
+    return prev;
   }
 
   /**
@@ -51,8 +51,9 @@ public class MyHashing {
    * @return (previous seed + value) % MODULO
    */
   public int hash(char value) {
-    // TODO
-    return 0;
+    int prev = this.seed;
+    this.seed = value;
+    return (prev + value) % MODULO;
   }
 
   /**
@@ -64,7 +65,11 @@ public class MyHashing {
    * @return the sum of the characters' numeric codes
    */
   public static int hash(String value) {
-    // TODO: String.toCharArray() may help.
-    return 0;
+    char[] array = value.toCharArray();
+    int sum = 0;
+    for (char c : array) {
+      sum += c;
+    }
+    return sum;
   }
 }
