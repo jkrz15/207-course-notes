e2a1d1c1bb21518bda9cc4c8c0e2f33034bfe8da
JK <janica.krznaric@mail.utoronto.ca>
2026-09-24 16:09:52 -0400

    implemented exercise 12

--- exercises/ex12-gotchas/src/main/java/Gotchas.java
+++ exercises/ex12-gotchas/src/main/java/Gotchas.java
@@ -28,8 +28,7 @@ public class Gotchas {
    * @param name the new name
    */
   public void setName(String name) {
-    // TODO: assign the parameter to the FIELD (hint: use `this`).
-    name = name;
+    this.name = name;
   }
 
   /**
@@ -41,9 +40,11 @@ public class Gotchas {
    * @return a copy whose inner arrays are also copies (nothing shared with grid)
    */
   public static int[][] deepCopy(int[][] grid) {
-    // TODO: build a new outer array and copy EACH inner array too, so that
-    //       nothing is shared with `grid`.
-    return grid.clone();
+    int[][] a = grid.clone();
+    for (int i = 0; i < grid.length; i++) {
+      a[i] = grid[i].clone();
+    }
+    return a;
   }
 
   /**
@@ -57,7 +58,6 @@ public class Gotchas {
    * @return true iff a and b hold the same int value
    */
   public static boolean sameValue(Integer a, Integer b) {
-    // TODO: compare the VALUES, not the references.
-    return a == b;
+    return a.equals(b);
   }
 }
